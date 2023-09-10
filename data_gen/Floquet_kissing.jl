8562624be08fe84934d1e87777e776298884ed64
Alex's mac <a.croscarrillo@gmail.com>
2023-09-10 12:09:15 -0400

    optimised f by making it inplance

--- data_gen/Floquet_kissing.jl
+++ data_gen/Floquet_kissing.jl
@@ -1,55 +1,54 @@
 include("../src/src.jl") # import src.jl which has creation/annahilation operators defined
 
-using LinearAlgebra # to diagonalise stuff
 using DataFrames # this is like pandas
 using CSV 
-using DifferentialEquations
 using ProgressBars
 
-function f(u, p, t)
-    # H, H_0, H_d, ω_d = p[1], p[2], p[3], p[4] 
-    H = p[1] + p[2]*cos(p[3]*t)
-    return -im * H * u
-end
+N = 100 #from the calculations in the eff model, N>=25 is sufficient 
+ω_0 = 1
+# g_n = [2*10^(-5),8*10^(-6)]
+g_n = [0.00075,1.27*10^(-7)]
+# g_n = [1,1]
+ω_d = 2*ω_0 #ignoring all the other terms
+# ω_d = ω_0 #ignoring all the other terms
 
-function f!(du,u, p, t)
-    # H, H_0, H_d, ω_d = p[1], p[2], p[3], p[4] 
-    H = p[1] + p[2]*cos(p[3]*t)
-    du .= -im * H * u
-end
 
-function quasienergies(N, ω_0, g_n, Ω_d, ω_d)
-    p =  H_0(N,ω_0,g_n), H_d(N,Ω_d), ω_d
-    T = 2*pi/ω_d
-    u_0 = ComplexF64.(Matrix(I(N)))
-    tspan = (0.0, 2*T)
-    prob = ODEProblem(f!, u_0, tspan, p)
-    sol = solve(prob, reltol = 1e-8, abstol = 1e-8)
-    U_2T = sol.u[end]
-    η_n, _ = eigen(U_2T)
-    ϵ_n = im*log.(η_n)/T
-    return real.(ϵ_n)
-end
+K = (10*g_n[1]^2)/(3*ω_0) - 3*g_n[2]/2
 
-N = 30 #from the calculations in the eff model, N>=25 is sufficient 
-ω_0 = 1.
-g_n = [10^(-5),10^(-6)]
-ω_d = 2*ω_0
-
-Ω_d_array = Vector(0:0.005:5) #graph above could be truncated at 10
+Ω_d_array = Vector(0:0.0001:1/10) #graph above could be truncated at 10
+# Ω_d_array = Vector(0:0.005:1) #graph above could be truncated at 10
 
 # Define data form
-data_array = zeros( N*length(Ω_d_array), 3 ) # data form: Δϵ_n | Ω_d | N
+data_array = zeros( N*length(Ω_d_array), 6 ) # data form: Δϵ_n | Ω_d | K | ϵ_2 | ϵ_2/K | N
+
+# The following is used to find ϵ_0 as described in the paper for your eyes only
+E_n , V_n = eigen(H_0(N,ω_0,g_n))
+V_0 = V_n[:, 1]
 
 counter = 1
-for (i,Ω_d) in ProgressBar(enumerate(Ω_d_array))
-    lamb = quasienergies(N, ω_0, g_n, Ω_d, ω_d)
+for (_, Ω_d) in ProgressBar(enumerate(Ω_d_array))
+    ϵ_n, ϕ_n = qen_qmodes(N, ω_0, g_n, Ω_d, ω_d)
+
+    overlaps = zeros(N)
     for n=1:N
-        data_array[counter,:] .= lamb[n]-minimum(lamb), Ω_d, N
+        overlaps[n] = real((V_0'*ϕ_n[:,n])*(ϕ_n[:,n]'*V_0))
+    end
+
+    ϵ_0 = ϵ_n[argmax(overlaps)]
+    V_0 = ϕ_n[:,argmax(overlaps)]
+    
+    ϵ_2 = 2*Ω_d*g_n[1]/(3*ω_0)
+    for n=1:N
+        data_array[counter,:] .= mod(ϵ_n[n] - ϵ_0, ω_d/2), Ω_d, K, ϵ_2, ϵ_2/K, N
         counter += 1
     end
+
+    # print("*")
+    # print(argmax(overlaps))
+    # print(", ")
+    # print(round(ϵ_2,digits=2))
 end
 
 # Put data in convenient DataFrame object
-df_floquet = DataFrame(data_array, ["Δϵ_n","Ω_d","N"]) 
+df_floquet = DataFrame(data_array, ["Δϵ_n","Ω_d","K","ϵ_2","ϵ_2/K","N"]) 
 CSV.write("data/floquet_kissing.csv", df_floquet)
