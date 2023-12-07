42d3fc9d7fcb4784e1ca135b96b0112948720f95
Alex's mac <a.croscarrillo@gmail.com>
2023-12-07 17:08:17 -0500

    woops didnt commit anything?

--- data_gen/Floquet_plus_H_eff/Crossings_animation_comparison.jl
+++ data_gen/Floquet_plus_H_eff/Crossings_animation_comparison.jl
@@ -6,7 +6,7 @@ using CSV
 using ProgressBars
 
 
-N = 70 #it works just fine for tests
+N = 150 #it works just fine for tests
 
 
 ##############
@@ -18,6 +18,8 @@ K = 1
 Δ = -0*K
 ϵ_1_array = Vector(range(0,14,length=200)).*K
 ϵ_2_array = Vector(range(0,18,length=200)).*K
+ϵ_1_array = ϵ_1_array[2:end]
+ϵ_2_array = ϵ_2_array[2:end]
 
 # Define data form: ΔE_n | ϵ_1 | ϵ_2 | Floquet?
 H_eff_data_array = zeros( N*length(ϵ_1_array)*length(ϵ_2_array), 4 ) 
@@ -47,12 +49,15 @@ CSV.write("data/h_eff_crossings_comparison.csv", df_h_eff)
 
 # Define Floquet parameter space
 ω_0 = 1
-g_n = [0.00075, 1.27*10^(-7)].*ω_0 # exact match with H_eff
-# g_n = [-0.000975, -0.0000133].*ω_0 # experimental values
+# g_n = [0.00075, 1.27*10^(-7)].*ω_0 # exact match with H_eff (garcia mata fig 1)
+# g_n = [-0.0035, -3.33e-5].*ω_0 #
+g_n = [-0.00331793, -5.0892e-5].*ω_0 # experimental values
 K = (10*g_n[1]^2)/(3*ω_0) - 3*g_n[2]/2
 
 Ω_1_array = Vector(range(0,Ω_1_max_by_ω_0(K,ϵ_1_array),length=200)).*ω_0 
 Ω_2_array = Vector(range(0,Ω_2_max_by_ω_0(K,g_n,ϵ_2_array),length=200)).*ω_0
+Ω_1_array = Ω_1_array[2:end]
+Ω_2_array = Ω_2_array[2:end]
 
 
 # Define data form: ΔE_n | ϵ_1 | ϵ_2 | Floquet?
@@ -80,14 +85,24 @@ for (i,_) in enumerate(Ω_1_array)
 
         # find closest groundstate and eigenergies and update
         ϵ_n, ϕ_n = qen_qmodes(N, ω_0, g_n, Ω_1, ω_1, Ω_2, ω_2)
-        overlaps = zeros(N)
-        for n=1:N
-            overlaps[n] = real( (V_0'*ϕ_n[:,n])*(ϕ_n[:,n]'*V_0) )
-        end
-        ϵ_0 = ϵ_n[argmax(overlaps)]
-        V_0 = ϕ_n[:,argmax(overlaps)]
+        # overlaps = zeros(N)
+        # for n=1:N
+        #     overlaps[n] = real( (V_0'*ϕ_n[:,n])*(ϕ_n[:,n]'*V_0) )
+        # end
+        # ϵ_0 = ϵ_n[argmax(overlaps)]
+        # V_0 = ϕ_n[:,argmax(overlaps)]
 
-        Δϵ_n = sort(mod.(ϵ_n .- ϵ_0, ω_2/2))
+        x_well, p_well = dblwell_minimas(K,ϵ_1,ϵ_2)[1], 0
+        coh_stt = coherent_state(N,x_well,p_well)
+
+        overlaps_array = norm.( (coh_stt'*ϕ_n) ) # get matrix of overlaps
+
+        levels_order_array = mapslices(argmax,overlaps_array,dims=2) # vector of levels positions, i.e. the groundstate position is levels_order_array[1].
+
+        ϕ_0 = ϕ_n[:,levels_order_array[1]]
+        ϵ_0 = ϵ_n[levels_order_array[1]]
+
+        Δϵ_n = sort( mod.( ϵ_n .- ϵ_0, ω_2/2) )
         #store parameters in array
         for n=1:N
             floquet_data_array[counter,:] .= Δϵ_n[n]/K, ϵ_1/K, ϵ_2/K, 1
--- misc/locura.jl
+++ misc/locura.jl
@@ -38,3 +38,77 @@ scatter!(lamb_test_1[1:20]./K_test_2,linestyle=:dash,ylim=(-100,5))
 
 
 # plot(lamb_test_1-lamb_test_2, ylim=(-5e-4,5e-4),xlim=(0,25))
+
+
+
+
+
+
+function H_cos(x,p,E_C,E_J,α,m,ϕ_ext)
+    H_kin = 4*E_C*(p)^2
+    return H_kin + potential(x,E_J,α,m,ϕ_ext)
+end
+
+function potential(x,E_J,α,m,ϕ_ext)
+    N = size(x)[1]
+    H_cos1 = - α*E_J*cos( x ) 
+    H_cos2 = - m*E_J*cos( I(N)*2*π*ϕ_ext/m - x/m )
+    return H_cos1 + H_cos2
+end
+
+function potential_classic(x,E_J,α,m,ϕ_ext)
+    H_cos1 = - α*E_J*cos( x ) 
+    H_cos2 = - m*E_J*cos( 2*π*ϕ_ext/m - x/m )
+    return H_cos1 + H_cos2
+end
+
+
+
+
+function H_charge_basis(N,m,α,ϕ_ext,E_C,E_J)
+    N_op = Diagonal(Vector(-N:1:N-1))
+
+    step = Matrix(Tridiagonal(repeat([0],2*N-1),repeat([0],2*N),repeat([1],2*N-1)))
+    step[1,end] = 0
+    step[end,1] = 1
+    display(step)
+    cos_m = (exp(-im*ϕ_ext*2*π)*step)^(1/m)
+    cos_m = (cos_m + cos_m')/2
+
+    cos_1 = Tridiagonal(repeat([1],2*N-1),repeat([0],2*N),repeat([1],2*N-1))/2
+
+    return 4*E_C*N_op^2 - E_J*(α*cos_1 + m*cos_m)
+end
+
+
+
+
+function taylor(f,order_n,x_0)
+    coeffs = zeros(order_n)
+    coeffs[1] = f(x_0)/factorial(1)
+    g_n = x -> f(x)
+    for n=2:order_n
+        g_n = x -> gradient(g_n,x)[1]
+        coeffs[n] = g_n(x_0)/factorial(n)
+    end
+    return coeffs
+end
+
+f_0(x) = potential_classic(x,5907,0.1,3,0.33)
+f_1(x) = gradient(f_0,x)[1]
+f_2(x) = gradient(f_1,x)[1]
+f_3(x) = gradient(f_2,x)[1]
+f_4(x) = gradient(f_3,x)[1]
+f_5(x) = gradient(f_4,x)[1]
+f_6(x) = gradient(f_5,x)[1]
+
+x_0 = 1.7794902664439491
+coeff_0 = f_0(x_0)/factorial(0)
+coeff_1 = f_1(x_0)/factorial(1)
+coeff_2 = f_2(x_0)/factorial(2)
+coeff_3 = f_3(x_0)/factorial(3)
+coeff_4 = f_4(x_0)/factorial(4)
+coeff_5 = f_5(x_0)/factorial(5)
+coeff_6 = f_5(x_0)/factorial(6)
+
+taylor(x) = coeff_0 + coeff_1*(x-x_0) + coeff_2*(x-x_0)^2+ coeff_3*(x-x_0)^3+ coeff_4*(x-x_0)^4+ coeff_5*(x-x_0)^5 + coeff_6*(x-x_0)^6
